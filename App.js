76ee8ab07d9742c3a237d202690c12f8f2537fca
josuejuca <josuejuca19@gmail.com>
2024-10-16 09:03:37 -0300

    [16/10] #2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
-
+  
 
 // Componentes importados
 import Welcome from './screens/Welcome'; 
